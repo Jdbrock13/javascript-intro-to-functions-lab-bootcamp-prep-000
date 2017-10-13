86ea1043dbb07aa069dea6b05b5ca59f6f3b3b04
Joshua Brock <jdbrock13@gmail.com>
2017-10-13 17:22:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+
+function sayHiToGrandma(string) {
+if (lowercase.toLowerCase() === lowercase) {
+return ("I can't hear you!")
+}
+else if (uppercase.toUpperCase() === uppercase) {
+  return ("YES INDEED")
+}
+}
+
+
+
+function sayHiToGrandma(string) {
+  uppercase.toUpperCase()
+  return ("YES INDEED!")
+}
